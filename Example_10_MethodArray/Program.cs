c89a8a174a5e1d33972d50a0824700e78b921541
Kedrorex <Belousovvk@bk.ru>
2023-02-12 14:12:01 +0400

    added Example_10_MetodArray

---
+++ Example_10_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 5, 14, 15, 45, 756};
+
+int n = array.Length;
+int find = 45;
+
+int i = 0;
+
+while (i < n)
+{
+  if (array[i] == find)
+  {
+    Console.WriteLine(i);
+    break;
+  }
+  i++;
+}
